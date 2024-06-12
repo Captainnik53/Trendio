3cd156ff451d8f095d05631c6c6006f48767012a
Nikunj <nikegondha987@gmail.com>
2024-06-12 20:59:32 +0530

    Added socket-io.client for frontend and integrated with backend server

--- backend/server.js
+++ backend/server.js
@@ -2,51 +2,94 @@ require('dotenv').config();
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const cheerio = require('cheerio');
 const axios = require('axios');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = socketIo(server, {
+    cors: {
+      origin: "http://localhost:3000", // Allow requests from this origin
+      methods: ["GET", "POST"],
+      credentials: true
+    }
+  });
 
 const BASE_API_URL = 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE'; // Replace with the actual API URL
 const API_TOKEN = process.env.API_TOKEN;
-const STOCK_SYMBOL = 'IBM'
-const API_URL = `${BASE_API_URL}&symbol=${STOCK_SYMBOL}&apikey=${API_TOKEN}`;
 
-// Function to check if the API is working
+const exchange = 'NASDAQ';
+// const API_URL = `${BASE_API_URL}&symbol=${STOCK_SYMBOL}&apikey=${API_TOKEN}`;
+
+// // Function to check if the API is working
 const checkApiStatus = async () => {
     try {
-        const response = await axios.get(API_URL);
-        if (response.status === 200) {
-            console.log('API is working properly');
-            console.log(response.data);
-        } else {
-            console.error(`API check failed with status: ${response.status}`);
-        }
+        const GOOGLE_FIN_API = `https://www.google.com/finance/quote/AMBUJACEM:NSE`;
+        const response = await axios.get(GOOGLE_FIN_API);
+        const html = response.data;
+        const $ = cheerio.load(html);
+
+        const targetDivText  = $('div.YMlKec.fxKbKc').text()
+        console.log(targetDivText);
+        console.log('finished');
     } catch (error) {
         console.error(`Error checking API: ${error.message}`);
     }
 };
 
-// Check the API status as soon as the server starts
-checkApiStatus();
+// // Check the API status as soon as the server starts
+// checkApiStatus();
+
+const fetchStockData = async (stockSymbol) => {
+    const GOOGLE_FIN_API = `https://www.google.com/finance/quote/${stockSymbol}:${exchange}`;
+    try {
+        console.log(GOOGLE_FIN_API);
+        const response = await axios.get(GOOGLE_FIN_API);
+        const html = response.data;
+        // console.log(html);
+        const $ = cheerio.load(html);
+
+        const targetDivText  = $('div.YMlKec.fxKbKc').text()
+        console.log(targetDivText);
+        return targetDivText;
+        // socket.emit('FromAPI', targetDivText);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+    }
+};
 
 io.on('connection', (socket) => {
     console.log('New client connected');
 
-    const fetchData = async () => {
-        try {
-            const response = await axios.get(API_URL);
-            socket.emit('FromAPI', response.data);
-        } catch (error) {
-            console.error(`Error: ${error.message}`);
-        }
-    };
+    
+    const intervals = {};
 
-    const interval = setInterval(fetchData, 5000); // Fetch data every 5 seconds
+    socket.on('startTracking', (stockSymbols) => {
+        console.log('startTracking', stockSymbols);
+        stockSymbols.forEach((symbol) => {
+            if (!intervals[symbol]) {
+                intervals[symbol] = setInterval(async () => {
+                    const data = await fetchStockData(symbol);
+                    if (data) {
+                        console.log('backend data', data);
+                        socket.emit('FromAPI', { symbol, data });
+                    }
+                }, 5000); // Fetch data every 5 seconds
+            }
+        });
+    });
+
+    socket.on('stopTracking', (stockSymbols) => {
+        stockSymbols.forEach((symbol) => {
+            clearInterval(intervals[symbol]);
+            delete intervals[symbol];
+        });
+    });
 
     socket.on('disconnect', () => {
-        clearInterval(interval);
+        Object.keys(intervals).forEach((symbol) => {
+            clearInterval(intervals[symbol]);
+        });
         console.log('Client disconnected');
     });
 });
--- frontend/src/components/Cards.js
+++ frontend/src/components/Cards.js
@@ -1,8 +1,15 @@
 // src/components/Cards.js
-import React from 'react';
 import styles from './Cards.module.css';
+import io from 'socket.io-client';
+import React, { useEffect, useState } from 'react';
 
-const Card = ({ logo, name, price, change, description }) => {
+
+const SOCKET_SERVER_URL = 'http://localhost:4000';
+
+
+let predefinedStockSymbols = []; 
+
+const Card = ({ logo, name, price, change, description, symbol }) => {
   return (
     <div className={styles.card}>
       <div className={styles.cardLogo}>{logo}</div>
@@ -16,17 +23,68 @@ const Card = ({ logo, name, price, change, description }) => {
 };
 
 const Cards = () => {
-  const stockData = [
-    { logo: '🔴', name: 'Amazon (AMZN)', price: '$279.9', change: '🔺10%', description: 'Apple Inc is designs, manufactures and markets mobile communication and media devices...' },
-    { logo: '🔵', name: 'Amazon (AMZN)', price: '$279.9', change: '🔺10%', description: 'Apple Inc is designs, manufactures and markets mobile communication and media devices...' },
-    { logo: '🔶', name: 'Amazon (AMZN)', price: '$279.9', change: '🔺10%', description: 'Apple Inc is designs, manufactures and markets mobile communication and media devices...' },
-    { logo: '⚫', name: 'Amazon (AMZN)', price: '$279.9', change: '🔺10%', description: 'Apple Inc is designs, manufactures and markets mobile communication and media devices...' },
-  ];
+  const socket = io(SOCKET_SERVER_URL);
+  predefinedStockSymbols = ['AAPL', 'GOOGL', 'NVDA', 'MSFT'];
+  // const stockData = [
+  //   { logo: '🔴', name: 'Amazon (AMZN)', price: '$279.9', change: '🔺10%', description: 'Apple Inc is designs, manufactures and markets mobile communication and media devices...' },
+  //   { logo: '🔵', name: 'Amazon (AMZN)', price: '$279.9', change: '🔺10%', description: 'Apple Inc is designs, manufactures and markets mobile communication and media devices...' },
+  //   { logo: '🔶', name: 'Amazon (AMZN)', price: '$279.9', change: '🔺10%', description: 'Apple Inc is designs, manufactures and markets mobile communication and media devices...' },
+  //   { logo: '⚫', name: 'Amazon (AMZN)', price: '$279.9', change: '🔺10%', description: 'Apple Inc is designs, manufactures and markets mobile communication and media devices...' },
+  // ];
 
+  const [stockData, setStockData] = useState(
+    {
+      'AAPL': { logo: '🔴', name: 'Amazon (AMZN)', price: '$279.9', change: '🔺10%', description: 'Apple Inc is designs, manufactures and markets mobile communication and media devices...', symbol: 'AAPL' },
+      'GOOGL': { logo: '🔵', name: 'Amazon (AMZN)', price: '$279.9', change: '🔺10%', description: 'Apple Inc is designs, manufactures and markets mobile communication and media devices...', symbol: 'GOOGL' },
+      'NVDA': { logo: '🔶', name: 'Amazon (AMZN)', price: '$279.9', change: '🔺10%', description: 'Apple Inc is designs, manufactures and markets mobile communication and media devices...', symbol: 'NVDA' },
+      'MSFT': { logo: '⚫', name: 'Amazon (AMZN)', price: '$279.9', change: '🔺10%', description: 'Apple Inc is designs, manufactures and markets mobile communication and media devices...', symbol: 'MSFT' }
+    });
+  
+
+  useEffect(() => {
+    // Connect to the socket server and start tracking predefined stocks
+    
+    socket.timeout(10000).emit('startTracking', predefinedStockSymbols, (err, responses) => {
+      if(err) {
+        console.log('error occurred', err);
+      }
+      else {
+        console.log('responses', responses);
+      }
+      
+    });
+   
+    
+
+    // Listen for real-time data updates from the server
+    socket.on('FromAPI', (data) => {
+      console.log('Data recieved from API: ', data);
+      setStockData((prevData) => {
+        const updatedData = {...prevData };
+        updatedData[data.symbol].price = data.data
+        return updatedData
+    });
+      console.log('stockData', stockData);
+    });
+
+    function onConnect() {
+      console.log('Connected to socket server');
+    }
+
+    socket.on('connect', onConnect);
+
+    // Clean up the socket connection on component unmount
+    return () => {
+      socket.emit('stopTracking', predefinedStockSymbols);
+      if(socket.connected)
+      socket.disconnect();
+    };
+  }, []);
+  
   return (
     <div className={styles.cardsContainer}>
-      {stockData.map((stock, index) => (
-        <Card key={index} {...stock} />
+      {predefinedStockSymbols.map((symbol) => (
+        <Card key={symbol} {...stockData[symbol]} />
       ))}
     </div>
   );
